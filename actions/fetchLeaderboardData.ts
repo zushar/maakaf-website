718ff416a3ace93b8c3f7e9fe52bf8632042fdfd
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-09-15 21:24:08 +0300

    refactor: Update API endpoints in fetchFilteredMemebers.ts and fetchLeaderboardData.ts
    Update the API endpoints in fetchFilteredMemebers.ts and fetchLeaderboardData.ts to use the production server URL (https://server.makkaf.xyz) instead of the previous URL (https://baas-data-provider.onrender.com). This ensures that the data is fetched from the correct server.
    
    Also, remove the unused local server URL in fetchLeaderboardData.ts.
    
    Fixes #issue_number

--- actions/fetchLeaderboardData.ts
+++ actions/fetchLeaderboardData.ts
@@ -7,10 +7,10 @@ import { Analitycs } from "@/app/[locale]/leaderboard/page";
 
   const LEADERBOARD_ROUTE =
     'https://baas-data-provider.onrender.com/leaderboard';
-  const LEADERBOARD_ROUTE_LOCAL = 'http://localhost:8080/leaderboard'; // If you want to use local server use
+  const LEADERBOARD_ROUTE_LOCAL = 'https://server.makkaf.xyz/leaderboard'; // If you want to use local server use
 
 async function fetchLeaderboard() {
-  const response = await fetch(LEADERBOARD_ROUTE)
+  const response = await fetch(LEADERBOARD_ROUTE_LOCAL)
 
   // fetch from endpoint POST with page, limit, filter as ILeaderboardResponse
   const data = await response.json() as unknown as Analitycs;
